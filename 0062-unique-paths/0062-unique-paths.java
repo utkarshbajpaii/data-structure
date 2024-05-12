2148a980f37e65a9a6bf1002929d672687d39a2e
Utkarsh Bajpai <160762548+utkarshbajpaii@users.noreply.github.com>
2024-05-12 12:39:51 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (42.53%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -4,20 +4,26 @@ class Solution {
         for(int[] row:dp){
             Arrays.fill(row,-1);
         }
-        return count(m-1,n-1,dp);
+        return count(m,n,dp);
     }
     public int count(int m,int n,int[][] dp){
-        if(m==0&n==0){
-            return 1;
-        }
-        if(m<0||n<0){
-            return 0;
-        }
-        if(dp[m][n]!=-1){
-            return dp[m][n];
-        }
-        int up=count(m-1,n,dp);
-        int left=count(m,n-1,dp);
-        return dp[m][n]=up+left;
+       for(int i=0;i<m;i++){
+           for(int j=0;j<n;j++){
+               if(i==0&&j==0){
+                    dp[i][j]=1;
+                   continue;
+               }
+               int up=0;
+               int left=0;
+               if(i>0){
+                   up=dp[i-1][j];
+               }
+               if(j>0){
+                   left=dp[i][j-1];
+               }
+               dp[i][j]=up+left;
+           }
+       }
+        return dp[m-1][n-1];
     }
 }
